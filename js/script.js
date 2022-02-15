d61e11a42581aeb9734993156551796af40e8c95
Luan Argolo <ipsluan@icloud.com>
2022-02-15 18:02:50 -0300

    Adicionar scroll suave

--- js/script.js
+++ js/script.js
@@ -40,3 +40,24 @@ function initAccordion() {
   });
 }
 initAccordion();
+
+function initScroll() {
+  const linksInterno = document.querySelectorAll('.js-menu a[href^="#"');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInterno.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScroll();
