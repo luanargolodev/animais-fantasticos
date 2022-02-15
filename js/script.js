919e9f80618634fbcb772bdba18f20127f0e8035
Luan Argolo <ipsluan@icloud.com>
2022-02-15 17:26:50 -0300

    Adicionar navegação por tab

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-// Duplique o menu e adicione ele em copy
-const menu = document.querySelector(".menu");
-const copy = document.querySelector(".copy");
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-const cloneMenu = menu.cloneNode(true);
-copy.appendChild(cloneMenu);
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo");
+}
 
-// Selecione o primeiro DT da dl de Faq
-const faq = document.querySelector(".faq");
-const primeiroDt = faq.querySelector("dt");
+function activeTab(index) {
+  tabContent.forEach((section) => {
+    section.classList.remove("ativo");
+  });
 
-// Selecione o DD referente ao primeiro DT
-const proximoDD = primeiroDt.nextElementSibling;
-console.log(proximoDD);
+  tabContent[index].classList.add("ativo");
+}
 
-// Substitua o conteúdo html de .faq pelo de .animais
-const animais = document.querySelector(".animais");
-faq.innerHTML = animais.innerHTML;
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener("click", () => {
+    activeTab(index);
+  });
+});
