767c81378db77cd3659e80dd7ba24893f0e99c20
Luan Argolo <ipsluan@icloud.com>
2022-02-15 15:00:52 -0300

    Resolução de exercícios

--- js/script.js
+++ js/script.js
@@ -1,41 +1,82 @@
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
-const imagem = document.querySelector("img");
-const imagemTop = imagem.offsetTop;
-console.log(imagemTop);
+const animaisLista = document.querySelector(".animais-lista");
 
-// Retorne a soma da largura de todas as imagens
-function somaImagens() {
-  const imagens = document.querySelectorAll("img");
-  let soma = 0;
-  imagens.forEach((imagem) => {
-    soma += imagem.offsetWidth;
-  });
-  return console.log(`A soma da largura de todas as imagens é ${soma}.`);
+function executarCallback(event) {
+  const currentTarget = event.currentTarget;
+  const target = event.target;
+  const type = event.type;
+  const path = event.path;
+  console.log(currentTarget, target, type, path);
 }
 
-window.onload = function () {
-  somaImagens();
-};
+animaisLista.addEventListener("click", executarCallback);
 
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-const links = document.querySelectorAll("a");
-links.forEach((link) => {
-  const linkWidth = link.offsetWidth;
-  const linkHeight = link.offsetHeight;
-  if (linkWidth >= 48 && linkHeight >= 48) {
-    console.log("Possui boa acessibilidade.");
-  } else {
-    console.log("Não possui boa acessibilidade.");
+const linkExterno = document.querySelector('a[href^="http"');
+
+function clickNoLink(event) {
+  event.preventDefault();
+  console.log(event.currentTarget.href);
+}
+
+linkExterno.addEventListener("click", clickNoLink);
+
+function handleKeyboard(event) {
+  if (event.key === "f") {
+    document.body.classList.toggle("fullscreen");
   }
+}
+
+window.addEventListener("keydown", handleKeyboard);
+
+function handleImg(event) {
+  console.log(event.target);
+}
+
+const imgs = document.querySelectorAll("img");
+imgs.forEach((img) => {
+  img.addEventListener("click", handleImg);
 });
 
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-const browserSmall = window.matchMedia("(max-width: 720px)").matches;
-if (browserSmall) {
-  const menu = document.querySelector(".menu");
-  menu.classList.add("menu-mobile");
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+const linksInternos = document.querySelectorAll('a[href^="#"');
+
+function handleLink(event) {
+  event.preventDefault();
+  linksInternos.forEach((link) => {
+    link.classList.remove("ativo");
+  });
+  event.currentTarget.classList.add("ativo");
 }
+
+linksInternos.forEach((link) => {
+  link.addEventListener("click", handleLink);
+});
+
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+const todosElementos = document.querySelectorAll("body *");
+
+todosElementos.forEach((elemento) => {
+  elemento.addEventListener("click", handleClickElemento);
+});
+
+function handleClickElemento(event) {
+  console.log(event.currentTarget);
+}
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+// function handleClickElemento(event) {
+//   event.currentTarget.remove();
+// }
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
+function handleClickT(event) {
+  if (event.key === "t") {
+    document.documentElement.classList.toggle("textomaior");
+  }
+}
+
+window.addEventListener("keydown", handleClickT);
