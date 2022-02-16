58c5c8fe11b78dac9c5a99fb386adc9f006e1db0
Luan Argolo <ipsluan@icloud.com>
2022-02-16 14:02:35 -0300

    Correção de exercícios

--- js/script.js
+++ js/script.js
@@ -87,39 +87,21 @@ function initAnimationScroll() {
 
 initAnimationScroll();
 
-// Crie uma função construtora de Pessoas
-// Deve conter nome, sobrenome e idade
-// Crie um método no protótipo que retorne
-// o nome completo da pessoa
-function Pessoa(nome, sobrenome, idade) {
-  this.nome = nome;
-  this.sobrenome = sobrenome;
-  this.idade = idade;
-}
+// Liste 5 objetos nativos
+Object;
+String;
+Array;
+Function;
+Number;
 
-Pessoa.prototype.nomeCompleto = function () {
-  return `${this.nome} ${this.sobrenome}`;
-};
+// Liste 5 objetos do browser
+window;
+history;
+Document;
+HTMLCollection;
+NodeList;
 
-const Luan = new Pessoa("Luan", "Argolo", 23);
-
-// Liste os métodos acessados por
-// dados criados com NodeList,
-// HTMLCollection, Document
-console.log(Object.getOwnPropertyNames(NodeList.prototype));
-console.log(Object.getOwnPropertyNames(HTMLCollection.prototype));
-console.log(Object.getOwnPropertyNames(Document.prototype));
-
-// Liste os construtores dos dados abaixo
-const li = document.querySelector("li");
-
-li; // HTMLLIElement
-li.click; // Function
-li.innerText; // String
-li.value; // Number
-li.hidden; // Boolean
-li.offsetLeft; // Number
-li.click(); // Undefined
-
-// Qual o construtor do dado abaixo:
-li.hidden.constructor.name; // String
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+document.webkitHidden;
+document.webkitVisibilityState;
