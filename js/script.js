f8a1bb51c75af610c6039d26168131d8ba3592be
Luan Argolo <ipsluan@icloud.com>
2022-02-20 12:21:18 -0300

    Novos exercícios

--- js/script.js
+++ js/script.js
@@ -87,21 +87,79 @@ function initAnimationScroll() {
 
 initAnimationScroll();
 
-// Liste 5 objetos nativos
-Object;
-String;
-Array;
-Function;
-Number;
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
 
-// Liste 5 objetos do browser
-window;
-history;
-Document;
-HTMLCollection;
-NodeList;
+const transacoes = [
+  {
+    descricao: "Taxa do Pão",
+    valor: "R$ 39",
+  },
+  {
+    descricao: "Taxa do Mercado",
+    valor: "R$ 129",
+  },
+  {
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 99",
+  },
+  {
+    descricao: "Taxa do Banco",
+    valor: "R$ 129",
+  },
+  {
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 49",
+  },
+];
 
-// Liste 2 Métodos, Propriedades ou Objetos
-// presentes no Chrome que não existem no Firefox
-document.webkitHidden;
-document.webkitVisibilityState;
+let valorTaxa = 0;
+let recebimentoTotal = 0;
+
+transacoes.forEach((item) => {
+  const numeroLimpo = +item.valor.replace("R$", "");
+  if (item.descricao.slice(0, 4) === "Taxa") {
+    valorTaxa += numeroLimpo;
+  } else {
+    recebimentoTotal += numeroLimpo;
+  }
+});
+console.log(valorTaxa);
+
+// Retorne uma array com a lista abaixo
+const transportes = "Carro;Avião;Trem;Ônibus;Bicicleta";
+const arrayTransportes = transportes.split(";");
+console.log(arrayTransportes);
+
+// Substitua todos os span's por a's
+let html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              </ul>`;
+html = html.split("span").join("a");
+console.log(html);
+
+// Retorne o último caracter da frase
+const frase = "Melhor do ano!";
+console.log(frase[frase.length - 1]);
+
+// Retorne o total de taxas
+const transacoes2 = [
+  "Taxa do Banco",
+  "   TAXA DO PÃO",
+  "  taxa do mercado",
+  "depósito Bancário",
+  "TARIFA especial",
+];
+
+let taxasTotal = 0;
+transacoes2.forEach((item) => {
+  item = item.toLowerCase();
+  item = item.trim();
+  item = item.slice(0, 4);
+  if (item === "taxa") {
+    taxasTotal++;
+  }
+});
+console.log(taxasTotal);
