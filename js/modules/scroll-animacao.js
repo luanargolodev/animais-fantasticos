8ce83fda9976dd408f951eebc61884dd74184f15
Luan Argolo <ipsluan@icloud.com>
2022-03-02 13:25:20 -0300

    Adicionar Modal no site

---
+++ js/modules/scroll-animacao.js
@@ -0,0 +1,21 @@
+export default function initAnimationScroll() {
+  const sections = document.querySelectorAll("[data-anime='scroll']");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animateScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      });
+    }
+
+    animateScroll();
+    window.addEventListener('scroll', animateScroll);
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
-// Adicione um atributo data-anime="show-down" e
-// data-anime="show-right" a todos as section's
-// com descricão dos animais.
+import initScroll from './modules/scroll-suave.js';
+import initAnimationScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
 
-// Utilizando estes atributos, adicione a classe
-// show-down ou show-right a sua respectiva section
-// assim que a mesma aparecer na tela (animacao tab)
-
-// No CSS faça com que show-down anime de cima para baixo
-// e show-right continue com a mesma animação da esquerda
-// para a direita
-
-// Substitua todas as classes js- por data atributes.
+initScroll();
+initAnimationScroll();
+initAccordion();
+initTabNav();
+initModal();
