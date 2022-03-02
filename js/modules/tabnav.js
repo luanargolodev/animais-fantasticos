e57b627dc674742b387b1db4524f726d13f59a20
Luan Argolo <ipsluan@icloud.com>
2022-03-02 14:27:28 -0300

    Adicionar tooltip no mapa

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -3,20 +3,20 @@ export default function initTabNav() {
   const tabContent = document.querySelectorAll("[data-tab='content'] section");
 
   if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("ativo");
+    tabContent[0].classList.add('ativo');
   }
 
   function activeTab(index) {
     tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+      section.classList.remove('ativo');
     });
 
     const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add("ativo", direcao);
+    tabContent[index].classList.add('ativo', direcao);
   }
 
   tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", () => {
+    itemMenu.addEventListener('click', () => {
       activeTab(index);
     });
   });
--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimationScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
 
 initScroll();
 initAnimationScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
