676611833e146c4bd3ae823fbb48dbc72b071d9e
Luan Argolo <ipsluan@icloud.com>
2022-03-03 00:57:21 -0300

    Adicionar outside click no menu

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,13 +1,18 @@
-export default function initDropDownMenu() {}
+import outsideClick from './outsideclick.js';
 
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-dropdownMenus.forEach((menu) => {
-  ['touchstart', 'click'].forEach((userEvent) => {
-    menu.addEventListener(userEvent, handleClick);
+export default function initDropDownMenu() {
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+  dropdownMenus.forEach((menu) => {
+    ['touchstart', 'click'].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
   });
-});
 
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.toggle('active');
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle('active');
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active');
+    });
+  }
 }
